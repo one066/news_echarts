ea94dde4fcec36c4537e0925cb16a90fdd941b63
one <49442264+one066@users.noreply.github.com>
2022-06-14 11:31:01 +0800

    更新代码 (#2)

--- src/apps/front_end/template_views.py
+++ src/apps/front_end/template_views.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Blueprint, render_template
 
 from extension.flask.api import login_require
@@ -11,56 +13,58 @@ front_end_views = Blueprint(
     static_folder='static'
 )
 
+record_number = os.environ.get("RECORD_NUMBER")
+
 
 @front_end_views.route('/home', methods=['GET'])
 @login_require
 def home():
 
-    return render_template('home.html')
+    return render_template('home.html', record_number=record_number)
 
 
 @front_end_views.route('/news', methods=['GET'])
 @login_require
 def news():
 
-    return render_template('news.html')
+    return render_template('news.html', record_number=record_number)
 
 
 @front_end_views.route('/chat', methods=['GET'])
 @login_require
 def chat():
 
-    return render_template('chat.html')
+    return render_template('chat.html', record_number=record_number)
 
 
 @front_end_views.route('/profile', methods=['GET'])
 @login_require
 def profile():
 
-    return render_template('profile.html')
+    return render_template('profile.html', record_number=record_number)
 
 
 @front_end_views.route('/show', methods=['GET'])
 @login_require
 def show():
 
-    return render_template('show.html')
+    return render_template('show.html', record_number=record_number)
 
 
 @front_end_views.route('/login', methods=['GET'])
 def login():
 
-    return render_template('login.html')
+    return render_template('login.html', record_number=record_number)
 
 
 @front_end_views.route('/admin_login', methods=['GET'])
 def admin_login():
 
-    return render_template('admin_login.html')
+    return render_template('admin_login.html', record_number=record_number)
 
 
 @front_end_views.route('/update_news', methods=['GET'])
 @login_require
 def update_news():
 
-    return render_template('update_news.html')
+    return render_template('update_news.html', record_number=record_number)
